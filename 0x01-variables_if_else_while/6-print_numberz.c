dc9b5bfcf21b69ba4387c1ffa70c0e8aa0bcd1a7
pappyjay <adeladiti2@gmail.com>
2022-12-12 20:54:24 -0800

    done

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Programming
+ *
+ * Return: Always 0
+ *
+ */
+
+int main(void)
+{
+	int a = 0;
+
+	for (; a < 10 ; a++)
+		putchar('0' + a);
+	putchar('\n');
+	return (0);
+}

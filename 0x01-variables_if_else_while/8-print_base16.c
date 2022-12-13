58f569464ee36e72432433ca6e0678e6000b3ee1
pappyjay <adeladiti2@gmail.com>
2022-12-12 21:02:00 -0800

    done

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+
+
+/**
+ * main - entry point
+ *
+ * Desciption: programming
+ *
+ * Return: always 0
+ *
+ */
+
+int main(void)
+{
+	unsigned char a = '0';
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar(a);
+		a++;
+	}
+	a = '1';
+	for (i = 0; i < 6; i++)
+	{
+		putchar('0' + a);
+		a++;
+	}
+	putchar('\n');
+	return (0);
+}

218ef58403b53d9e76a7dc81b042c6a7550d9852
pappyjay <adeladiti2@gmail.com>
2022-12-16 17:54:38 -0800

    done

---
+++ 0x03-debugging/0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,17 @@
+#ifdef MAIN_H
+#define MAIN_H
+
+int _putchar(char c);
+int _isupper(int c);
+int _isdigit(int c);
+int mul(int a, int b);
+void print_numbers(void);
+void print_most_numbers(void);
+void more-numbers(void);
+void print_lines(int n);
+void print_diagonal(int n);
+void print_square(int size);
+void print_triangle(int size);
+void print_number(int n);
+
+#endif
